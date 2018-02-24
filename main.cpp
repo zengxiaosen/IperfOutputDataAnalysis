67fae26fb6418d659e0da16bf692322a4e556d78
zengxiaosen <919451814@qq.com>
2018-02-24 12:28:05 +0800

    test

--- main.cpp
+++ main.cpp
@@ -175,6 +175,7 @@ map<string, zbInfoNode>  ReadDataFromFileLBLIntoCharArray(string s)
             zb.setAllpackets(strs[12]);
             //cout << strs[11] << endl;
             zb.setLosspackets(strtok((char *) strs[11].c_str(), "/"));
+            map.insert(pair<string, zbInfoNode>(p, zb));
         }
 
 
@@ -182,6 +183,7 @@ map<string, zbInfoNode>  ReadDataFromFileLBLIntoCharArray(string s)
         j++;
     }
 
+    //cout << map.size() << endl;
     return map;
 
 }
@@ -212,8 +214,12 @@ void readQueue(queue<string> &queue) {
         if(e.find("client") == -1){
             //没找到，即为server类型
             map<string, zbInfoNode> map = ReadDataFromFileLBLIntoCharArray(frontstring+e);
-            //map.size() = 60
-            //cout << map.size() << endl;
+            /**
+             * map.size() = 51 而不是 60 待修复
+             * status: 已修复
+             */
+
+            //cout << "map.size()" << map.size() << endl;
 
             /**
              * key:第几秒
@@ -222,7 +228,7 @@ void readQueue(queue<string> &queue) {
             for(const pair<string, zbInfoNode> & maplocal : map){
                 string key = maplocal.first;
                 zbInfoNode value = maplocal.second;
-                cout << "key: " << key << endl;
+//                cout << "key: " << key << endl;
 //                cout << "value: " << endl;
                 /**
                  * 这里注意，10以下的key解析出现字段错位
@@ -234,6 +240,10 @@ void readQueue(queue<string> &queue) {
                     status:已修复
                  */
 //                value.toString();
+
+                /**
+                 * 对bandwith进行秒级别的聚合
+                 */
                 if(index_bandwitdh[key] == 0 || index_bandwitdh[key] == NULL){
                     index_bandwitdh[key] = 0;
                 }
@@ -244,17 +254,48 @@ void readQueue(queue<string> &queue) {
                 int tempsum_bandwith = base_bandwith + add_bandwith;
 
                 index_bandwitdh[key] = tempsum_bandwith;
-                cout << index_bandwitdh[key] << endl;
+                //cout << index_bandwitdh[key] << endl;
+
+
+                /**
+                 * 对droppackets进行秒级别聚合
+                 */
+                if(index_droppackets[key] == 0 || index_droppackets[key] == NULL){
+                    index_droppackets[key] = 0;
+                }
+                int base_droppackets = index_droppackets[key];
+                int add_droppackets = atoi(value.getLosspackets().c_str());
+                int tempsum_droppackets = base_droppackets + add_droppackets;
+                index_droppackets[key] = tempsum_droppackets;
+                //cout << index_droppackets[key] << endl;
+
+
+                /**
+                 * 对allpackets进行秒级别的聚合
+                 */
+                if(index_allpackets[key] == 0 || index_allpackets[key] == NULL){
+                    index_allpackets[key] = 0;
+                }
+                int base_allpackets = index_allpackets[key];
+                int add_allpackets = atoi(value.getAllpackets().c_str());
+                int tempsum_allpackets = base_allpackets + add_allpackets;
+                index_allpackets[key] = tempsum_allpackets;
+                //cout << index_allpackets[key] << endl;
+
 
 
 
             }
             cout << "==========" << endl;
 
+
             queue.pop();
         }else{
             //找到client标识，即为client类型
-            queue.pop();
+            //queue.pop();
+            /**
+             * client类型的node放到别的方法,这块废弃
+             */
         }
 
 
@@ -263,7 +304,18 @@ void readQueue(queue<string> &queue) {
 
     /**
      * test 三个统计的hashmap
+     * 得到1分钟内每秒的所有流的带宽信息，丢包信息，总包数信息
      */
+    cout << "index_bandwitdh.size(): " << index_bandwitdh.size() << endl;
+    cout << "index_droppackets.size(): " << index_droppackets.size() << endl;
+    cout << "index_allpackets.size(): " << index_allpackets.size() << endl;
+
+    /**
+     * 写到数据库
+     * springboot或django展示
+     */
+
+
 
 
     if(queue.empty()){
@@ -307,7 +359,7 @@ int main() {
     map<string, zbInfoNode> index_infoServerNode;
 
     readQueue(q_server);
-    readQueue(q_client);
+    //readQueueClient(q_client);
 
 
 
